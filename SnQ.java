5d454313fa286d08116c742dae040ffd870d5957
DESKTOP-UGN8TA5\Emoney <ethornton@albany.edu>
2016-10-30 20:33:47 -0400

    Firing the program up for the first time

---
+++ SnQ.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snq;
+
+import javafx.scene.Node;
+
+
+/**
+ *
+ * @author Emoney
+ */
+public class SnQ {
+    
+    public class Stack{
+         Node head;
+        
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
