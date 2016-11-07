df12a9b1c33d2af2923eac89c0704722050bddec
DESKTOP-UGN8TA5\Emoney <ethornton@albany.edu>
2016-11-06 19:50:11 -0500

    Moving onto my node class

---
+++ Node.java
@@ -0,0 +1,14 @@
+
+public class Node
+    {
+        int lol;
+        Node next;
+        
+        //Node Constructor
+        Node(int d)
+        {
+            lol = d;
+            next = null;
+        }
+        
+    }
